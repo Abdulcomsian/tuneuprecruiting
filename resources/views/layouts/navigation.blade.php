4022b6f6a7cb48b09e380f7dc7d7adce4a4ab4f6
Shoukat Mirza <shoukat.bjr@gmail.com>
2023-11-06 18:32:35 +0500

    navbar for students

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar-wrapper" data-layout="stroke-svg">
     <div>
-        <div class="logo-wrapper"><a href="{{ url('/dashboard') }}"><img class="img-fluid" src="{{ asset('assets/images/logo/logo.png') }}" alt=""></a>
+        <div class="logo-wrapper"><a><img class="img-fluid" src="{{ asset('assets/images/logo/logo.png') }}" alt=""></a>
             <div class="back-btn"><i class="fa fa-angle-left">   </i></div>
         </div>
         <nav class="sidebar-main">
@@ -35,32 +35,24 @@
                 @php endif; @endphp
 
                 @php if (auth()->user()->role == 'student'): @endphp
-                <ul class="sidebar-links" id="simple-bar">
-                    <li class="back-btn"><a href="{{ url('/dashboard') }}"></a>
-                        <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
-                    </li>
-                    <li class="sidebar-main-title">
-                        <div></div>
-                    </li>
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title" href="{{ url('/dashboard') }}">
-                            <svg class="stroke-icon">
-                                <use href="{{ asset('assets/svg/icon-sprite.svg#home') }}"></use>
-                            </svg>
-                            <svg class="fill-icon">
-                                <use href="{{ asset('assets/svg/icon-sprite.svg#fill-home') }}"></use>
-                            </svg><span>Home</span>
-                        </a>
-                    </li>
-                    <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="{{ url('applies') }}">
-                            <svg class="stroke-icon">
-                                <use href="{{ asset('assets/svg/icon-sprite.svg#Ui-kites') }}"></use>
-                            </svg>
-                            <svg class="fill-icon">
-                                <use href="{{asset('assets/svg/icon-sprite.svg#fill-ui')}}"></use>
-                            </svg><span>Applies</span></a>
-                    </li>
-                </ul>
+                    <ul class="sidebar-links" id="simple-bar">
+                        <li class="back-btn"><a href="{{ url('student/dashboard') }}"></a>
+                            <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
+                        </li>
+                        <li class="sidebar-main-title">
+                            <div></div>
+                        </li>
+                        <li class="sidebar-list">
+                            <a class="sidebar-link sidebar-title" href="{{ url('student/dashboard') }}">
+                                <svg class="stroke-icon">
+                                    <use href="{{ asset('assets/svg/icon-sprite.svg#home') }}"></use>
+                                </svg>
+                                <svg class="fill-icon">
+                                    <use href="{{ asset('assets/svg/icon-sprite.svg#fill-home') }}"></use>
+                                </svg><span>Home</span>
+                            </a>
+                        </li>
+                    </ul>
                 @php endif; @endphp
             </div>
         </nav>
--- resources/views/student_backend/dashboard/dashboard.blade.php
+++ resources/views/student_backend/dashboard/dashboard.blade.php
@@ -9,7 +9,7 @@
                     <div class="page-title mt-2">
                         <div class="row">
                             <div class="col-sm-6 ps-0">
-                                <h3>CRM Dashboard</h3>
+                                <h3>Student Dashboard</h3>
                             </div>
                             <div class="col-sm-6 pe-0">
                                 <ol class="breadcrumb">
