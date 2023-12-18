2bf89f59a00d68913051205804e82db5dfa1208d
Shoukat Mirza <shoukat.bjr@gmail.com>
2023-12-18 16:59:14 +0500

    change footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,14 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 footer-copyright">
-                <p class="mb-0">Copyright 2023 © Cion theme by pixelstrap.</p>
-            </div>
-            <div class="col-md-6">
-                <p class="float-end mb-0">Hand crafted &amp; made with
-                    <svg class="footer-icon">
-                        <use href="../assets/svg/icon-sprite.svg#heart"></use>
-                    </svg>
-                </p>
+                <p class="mb-0">Copyright 2023 © Tune Up Recruiting.</p>
             </div>
         </div>
     </div>
