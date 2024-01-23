94a50862521fd8092337cad828f69522bfceee82
Shoukat Mirza <shoukat.bjr@gmail.com>
2024-01-23 17:08:15 +0500

    fix twice notification issue.

--- resources/views/backend/applies/accept.blade.php
+++ resources/views/backend/applies/accept.blade.php
@@ -62,14 +62,6 @@
                                     </ul>
                                 </div>
                             @endif
-
-                            @if(session('success') || session('danger'))
-                                @php $className = (session('success')) ? 'success' : 'danger'; @endphp
-                                @php $message = (session('success')) ? session('success') : session('danger'); @endphp
-                                <div class="alert alert-{{ $className }}">
-                                    {{ $message }}
-                                </div>
-                            @endif
                             @if($apply->status == 'accepted')
                                 <div class="alert alert-success">
                                     Approved. The notification has been dispatched to the student.
