c52a3d4c6fe83d0657a4262570a36056a6f1d1ab
abidali05 <abid.ali@accrualhub.com>
2024-06-26 17:23:17 +0500

    store image
    store image

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -116,13 +116,13 @@ class MediaController extends Controller
     public function storeImage(Request $request)
     {
         try {
-            $validator = Validator::make($request->all(), [
-                'media_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-            ]);
+            // $validator = Validator::make($request->all(), [
+            //     'media_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            // ]);
 
-            if ($validator->fails()) {
-                return redirect()->back()->withErrors($validator)->withInput();
-            }
+            // if ($validator->fails()) {
+            //     return redirect()->back()->withErrors($validator)->withInput();
+            // }
 
             $filename = uniqid('image_') . '.' . $request->media_image->getClientOriginalExtension();
 
@@ -141,7 +141,7 @@ class MediaController extends Controller
                 'link' => $link,
             ]);
 
-            dd($mediaImage);
+            dd($mediaImage, $request->all());
 
             return redirect()->back()->with('success', 'Image stored successfully.');
         } catch (\Exception $e) {
